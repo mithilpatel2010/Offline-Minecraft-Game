6e1cc98cbc026097487b0ec331ec3eb117eea7d2
mithilpatel2010 <103128890+mithilpatel2010@users.noreply.github.com>
2022-04-06 16:21:24 +0100

    Add files via upload

---
+++ forgot.js
@@ -0,0 +1,13 @@
+function back(){
+    window.location = "index.html";
+}
+
+function yes()
+{
+    window.location = "mithil.html"
+}
+
+function no()
+{
+    window.location = "password.html"
+}
---
+++ main.js
@@ -0,0 +1,21 @@
+function addUser()
+{
+    var numbers = 123456;
+    var numbers1 = 0;
+    if(Password.value.match(numbers))
+    {
+    window.location = "minecraft.html";
+    alert("You have been succesfuly logged in! Press OK to be redirected to the Minecraft Game.")
+    }
+    else if(Password.value.match(numbers1))
+    {
+        alert("Invalid Password, please try again.")
+        window.location = "index.html"
+    }
+}
+
+function forgot()
+{
+    window.location = "forgot.html"
+    alert("Press OK to be redirected to the Forgot Password page.")
+}
